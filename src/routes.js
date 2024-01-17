78f87a7c448d9e689865422fc25078c5f3378f83
Pedro Giamarco <pedrodaniel0020@gmail.com>
2024-01-17 15:52:58 -0300

    change endpoint name

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const routes = express.Router();
 
 const chordController = require('./controllers/chordController');
 
-routes.post('/insert', chordController.store);
+routes.post('/create', chordController.store);
 routes.put('/update', chordController.update);
 routes.get('/read', chordController.index);
 routes.get('/details/:_id',chordController.detail);
